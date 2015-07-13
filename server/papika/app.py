d0022ea3cdefb7803a67ded20731959b8da73d26
edbutler <zantifon@gmail.com>
2015-07-13 16:05:41 -0700

    pulled models into separate module.
    This will allow other scripts besides app.py to access the DB schema.

--- server/papika/app.py
+++ server/papika/app.py
@@ -2,15 +2,15 @@ import os, sys
 import json
 import uuid
 import random
-import flask, flask.ext.sqlalchemy, flask_cors
-from sqlalchemy.dialects.postgresql import UUID
+import flask, flask_cors
 import datetime, dateutil.parser
 import hashlib
+from .model import db, User, Session, Event, TaskStart, TaskEvent, UserExperiment
 
 app = flask.Flask(__name__)
 app.config.from_object('papika.defaultconfig')
 app.config.from_envvar('PAPIKA_CONFIG', silent=True)
-db = flask.ext.sqlalchemy.SQLAlchemy(app)
+db.init_app(app)
 cors = flask_cors.CORS(app)
 experiments = app.config['PAPIKA_EXPERIMENTS']
 
@@ -22,74 +22,6 @@ def set_from_dict(self, items):
         if n in items:
             setattr(self, n, items[n])
 db.Model.set_from_dict = set_from_dict
-
-class Session(db.Model):
-    id = db.Column(UUID, primary_key=True)
-    # user id, specified by client, used to match up sessions with the same user
-    user_id = db.Column(UUID, nullable=False)
-    # the application version
-    release_id = db.Column(UUID, nullable=False)
-    # wall time the server received the request to log this event
-    server_time = db.Column(db.DateTime(timezone=True), nullable=False)
-    # wall time of the client computer when the event was generated
-    client_time = db.Column(db.DateTime(timezone=True), nullable=False)
-    # blob of json data
-    detail = db.Column(db.Unicode, nullable=False)
-    # the VCS revision id of the telemetry library (uniquely identifying which version is running)
-    library_revid = db.Column(db.Unicode, nullable=False)
-
-class Event(db.Model):
-    id = db.Column(db.BigInteger, primary_key=True)
-    session_id = db.Column(Session.id.type, db.ForeignKey('session.id'), nullable=False)
-    # index of this event in all events this session (e.g., 1st event is 1, 2nd is 2...)
-    session_sequence_index = db.Column(db.Integer, nullable=False)
-    # wall time the server received the request to log this event
-    server_time = db.Column(db.DateTime(timezone=True), nullable=False)
-    # wall time of the client computer when the event was generated
-    client_time = db.Column(db.DateTime(timezone=True), nullable=False)
-    # the "type" of the event. This is entirely application specific, and is used
-    # by aplications to partition events into different kinds.
-    type_id = db.Column(db.SmallInteger, nullable=False)
-    # blob of json data
-    detail = db.Column(db.Unicode, nullable=False)
-
-# a type of event which marks the start of a group of events.
-# other events can be associated with this event.
-# used to group events that are part of the same game level, problem, puzzle, task, quest, etc.
-class TaskStart(db.Model):
-    __tablename__ = 'task_start'
-    id = db.Column(Event.id.type, db.ForeignKey('event.id'), primary_key=True)
-    # client-selected id for this task. all events within the task will share this id.
-    task_id = db.Column(db.Integer, nullable=False)
-    # aka level id, problem id, quest id, puzzle id. whatever the things you are tracing are.
-    group_id = db.Column(UUID, nullable=False)
-
-class TaskEvent(db.Model):
-    __tablename__ = 'task_event'
-    id = db.Column(Event.id.type, db.ForeignKey('event.id'), primary_key=True)
-    # client-selected id for this task. all events within the task will share this id.
-    task_id = db.Column(TaskStart.task_id.type, nullable=False)
-    # index of this event in all events this task (e.g., 1st event is 1, 2nd is 2...)
-    task_sequence_index = db.Column(db.Integer, nullable=False)
-
-# mapping from indenifiable usernames to (presumably anonymous) user ids.
-class User(db.Model):
-    # the user id, will be generated
-    id = db.Column(UUID, primary_key=True)
-    # the username
-    username = db.Column(db.Unicode, unique=True, nullable=False)
-    # json blob of arbitrary queryable "save data"
-    savedata = db.Column(db.Unicode)
-
-# many-to-many mapping of users to experimental condition
-class UserExperiment(db.Model):
-    __tablename__ = 'user_experiment'
-    # user id, might not be in the user table if the client is generating its own user ids
-    user_id = db.Column(UUID, primary_key=True)
-    # experiement id, specified in the config file
-    experiment_id = db.Column(UUID, primary_key=True)
-    # condition to which they are assigned, chocies are specified in the config file
-    condition = db.Column(db.Integer, nullable=False)
 
 def parse_message(request):
     content = request.json
@@ -219,5 +151,6 @@ def get_experimental_condition():
     return flask.jsonify(condition=pe.condition)
 
 def setup():
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
